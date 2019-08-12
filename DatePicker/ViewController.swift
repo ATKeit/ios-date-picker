a6fefedaf2403e4a3514caaa1a428b7f1253a5a2
ATHeart <b.angelthink@gmail.com>
2019-08-12 20:48:58 +0900

    Added DatePicker
    - Added label for current date and selected date
    - Added date picker

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblCurrentDt: UILabel!
+    @IBOutlet weak var lblSelectedDt: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let date = Date()
+        let currentDt = DateFormatter()
+        currentDt.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        lblCurrentDt.text = currentDt.string(from: date)
     }
 
-
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let selectedDt = DateFormatter()
+        selectedDt.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        lblSelectedDt.text = selectedDt.string(from: datePickerView.date)
+    }
+    
 }
 
